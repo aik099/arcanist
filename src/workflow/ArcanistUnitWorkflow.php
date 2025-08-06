23b908beb71cfc2776fac345ea5867861c6cc80b
Alex <aik099@users.noreply.github.com>
2025-08-06 18:55:52 +0300

    Disable code coverage collection by default, because it's slow and tailored to Phabricator test structure

--- src/workflow/ArcanistUnitWorkflow.php
+++ src/workflow/ArcanistUnitWorkflow.php
@@ -166,7 +166,8 @@ EOTEXT
     $renderer = new ArcanistUnitConsoleRenderer();
     $this->engine->setRenderer($renderer);
 
-    $enable_coverage = null; // Means "default".
+    //$enable_coverage = null; // Means "default".
+    $enable_coverage = false; // Disable code coverage by default.
     if ($this->getArgument('coverage') ||
         $this->getArgument('detailed-coverage')) {
       $enable_coverage = true;
